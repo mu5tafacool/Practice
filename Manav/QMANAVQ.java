e30feec07534e358c123d07c98d24465682f62d3
mu5tafacool <100384714+mu5tafacool@users.noreply.github.com>
2022-04-20 12:56:34 +0300

    12:56

--- Manav/QMANAVQ.java
+++ Manav/QMANAVQ.java
@@ -11,21 +11,20 @@ public class QMANAVQ extends Mgilgi {
         System.out.println(urunler);
         System.out.print("lutfen urun secin: ");
 
-      //  int mSecim = scan.nextInt();
-
-
-       int mSecim = 0;
-        try {
-            mSecim = scan.nextInt();
-        }  catch (InputMismatchException e) {
-
-        }
+        int mSecim = scan.nextInt();
         urunHesaplama(mSecim, urunler);
 
+//tek problem harf girildiginde
+        /***** Alternatif
+         int mSecim = 0;
+         try {
+         mSecim = scan.nextInt();
+         }  catch (InputMismatchException e) {
 
-
+         }
+         urunHesaplama(mSecim, urunler);
+         ******/
     }
 
 
 }
-
